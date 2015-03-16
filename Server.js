0693d1713d05be8aace7d443682cf6283934d8ca
Rafael Escobar <rafael.bze@gmail.com>
2015-03-16 16:38:57 -0300

    Add semicolons and reindent the code.

--- Server.js
+++ Server.js
@@ -1,18 +1,18 @@
 var http = require("http");
 var fs = require("fs");
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var createResponse = function (error, response, data, contentType) {
-    if(error) {
-        response.writeHead(404, {"Content-type": "text/plain"});
+    if (error) {
+        response.writeHead(404, { "Content-type": "text/plain" });
         response.end("File not found.");
     }
 
     else {
-    response.writeHead(200, { "Content-Type": contentType });
-    response.end(data);
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(data);
     }
 };
 
@@ -23,28 +23,27 @@ var server = http.createServer(function (request, response) {
         response.end();
     }
     else {
-
-        fs.readFile(__dirname + '/Index.html', function(error, html) {
+        fs.readFile(__dirname + '/Index.html', function (error, html) {
             createResponse(error, response, html, "text/html")
         });
 
-        fs.readFile(__dirname + '/Style.css', function(error, css) {
+        fs.readFile(__dirname + '/Style.css', function (error, css) {
             createResponse(error, response, css, "text/css")
         });
 
-        fs.readFile(__dirname + '/Media/Profile.jpg', function(error, image) {
+        fs.readFile(__dirname + '/Media/Profile.jpg', function (error, image) {
             createResponse(error, response, image, "image/jpeg")
         });
 
-        fs.readFile(__dirname + '/Media/Background/Stars.png', function(error, image) {
+        fs.readFile(__dirname + '/Media/Background/Stars.png', function (error, image) {
             createResponse(error, response, image, "image/png")
         });
 
-        fs.readFile(__dirname + '/Media/Background/Stripes.png', function(error, image) {
+        fs.readFile(__dirname + '/Media/Background/Stripes.png', function (error, image) {
             createResponse(error, response, image, "image/png")
         });
 
-        fs.readFile(__dirname + '/Media/Background/Texture.png', function(error, image) {
+        fs.readFile(__dirname + '/Media/Background/Texture.png', function (error, image) {
             createResponse(error, response, image, "image/png")
         });
     }
