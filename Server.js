863508aa06b312fa3bf6dc5e4be3a63f799615f6
Rafael Escobar <rafael.bze@gmail.com>
2015-03-15 23:37:50 -0300

    Update Openshift configurations.

--- Server.js
+++ Server.js
@@ -1,6 +1,9 @@
 var http = require("http");
 var fs = require("fs");
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 var server = http.createServer(function (request, response) {
     if (request.url == "/en") {
         response.writeHead(200, { "Content-Type": "text/html" });
@@ -14,6 +17,6 @@ var server = http.createServer(function (request, response) {
     }
 });
 
-server.listen(80, function () {
-    console.log("Rodando na porta 80.");
+server.listen(server_port, server_ip_address, function () {
+    console.log("Running on " + server_ip_address + ":" + server_port + ".");
 });
