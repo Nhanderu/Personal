95c7171450e890cabfeb144d80a88d5d8e390c6f
Rafael Escobar <rafael.bze@gmail.com>
2015-03-15 23:40:09 -0300

    Add root path on server.js.

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ var server = http.createServer(function (request, response) {
         response.write("<h1>Hello, world!</h1>");
         response.end();
     }
-    else if (request.url == "/en") {
+    else {
         response.writeHead(200, { "Content-Type": "text/html" });
         response.write("<h1>Olá, mundo!</h1>");
         response.end();
