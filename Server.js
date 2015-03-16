6fc64bea3fd44d74417c874b957421ee974c4cc8
Rafael Escobar <rafael.bze@gmail.com>
2015-03-16 17:18:13 -0300

    Fix async error.

--- Server.js
+++ Server.js
@@ -1,23 +1,9 @@
 var http = require("http");
 var fs = require("fs");
+var async = require('async');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-var createResponse = function (fs, response, path, contentType) {
-    fs.readFile(path, function (error, data) {
-        if (error) {
-            response.writeHead(404, { "Content-type": "text/plain" });
-            response.end("File not found.");
-        }
-
-        else {
-            response.writeHead(200, { "Content-Type": contentType });
-            response.write(data);
-            response.end(data);
-        }
-    });
-};
 
 var server = http.createServer(function (request, response) {
     if (request.url == "/en") {
@@ -26,12 +12,33 @@ var server = http.createServer(function (request, response) {
         response.end();
     }
     else {
-        createResponse(fs, response, __dirname + '/Index.html', "text/html");
-        createResponse(fs, response,__dirname + '/Style.css', "text/css");
-        createResponse(fs, response, __dirname + '/Media/Profile.jpg', "image/jpeg");
-        createResponse(fs, response,__dirname + '/Media/Background/Stars.png', "image/png");
-        createResponse(fs, response,__dirname + '/Media/Background/Stripes.png', "image/png");
-        createResponse(fs, response,__dirname + '/Media/Background/Texture.png', "image/png");
+        var count = 0;
+        var handler = function (contentType) {
+            return function (error, data) {
+                count++;
+
+                if (error) {
+                    response.writeHead(404, { "Content-type": "text/plain" });
+                    response.end("File not found.");
+                }
+
+                else {
+                    response.writeHead(200, { "Content-Type": contentType });
+                    response.write(data);
+                }
+
+                if (count == 6) {
+                    respo.end();
+                }
+            };
+        };
+
+        fs.readFile(__dirname + "/Index.html", handler("text/html"));
+        fs.readFile(__dirname + "/Style.css", handler("text/css"));
+        fs.readFile(__dirname + "/Media/Profile.jpg", handler("image/jpeg"));
+        fs.readFile(__dirname + "/Media/Background/Stars.png", handler("image/png"));
+        fs.readFile(__dirname + "/Media/Background/Stripes.png", handler("image/png"));
+        fs.readFile(__dirname + "/Media/Background/Texture.png", handler("image/png"));
     }
 });
 
