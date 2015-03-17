850e1004bb5160804ea6ebf0699de802cec44087
Rafael Escobar <rafael.bze@gmail.com>
2015-03-17 18:39:30 -0300

    Write head more than once.

--- Server.js
+++ Server.js
@@ -13,17 +13,26 @@ var server = http.createServer(function (request, response) {
     }
     else {
         response.writeHead(200, { "Content-Type": "text/html" });
-
         var index = fs.readFileSync(__dirname + "/Index.html");
         response.write(index);
+
+        response.writeHead(200, { "Content-Type": "text/css" });
         var style = fs.readFile(__dirname + "/Style.css");
         response.write(style);
+
+        response.writeHead(200, { "Content-Type": "image/jpeg" });
         var imageProfile = fs.readFile(__dirname + "/Media/Profile.jpg");
         response.write(imageProfile);
+
+        response.writeHead(200, { "Content-Type": "image/png" });
         var imageStars = fs.readFile(__dirname + "/Media/Background/Stars.png");
         response.write(imageStars);
+
+        response.writeHead(200, { "Content-Type": "image/png" });
         var imageStripes = fs.readFile(__dirname + "/Media/Background/Stripes.png");
         response.write(imageStripes);
+
+        response.writeHead(200, { "Content-Type": "image/png" });
         var imageTexture = fs.readFile(__dirname + "/Media/Background/Texture.png");
         response.write(imageTexture);
 
