255caa093e2748458cdfc4f5c3588b30dadb8dba
Rafael Escobar <rafael.bze@gmail.com>
2015-04-03 13:38:42 -0300

    Change the port to a string.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var router = require("./routes/index");
 var app = express();
 
 // Express settings.
-app.set("port", process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set("port", process.env.OPENSHIFT_NODEJS_PORT || "8080");
 app.set("ip address", process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
