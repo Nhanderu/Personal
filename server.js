ff9803f96dc175f833bc442726523c79a62bb579
Rafael Escobar <rafael.bze@gmail.com>
2015-04-20 16:33:18 -0300

    Reorganize code.

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
+// Imports the necessary modules.
 var express = require("express");
 var handlebars = require("express-handlebars");
-var router = require("./routes/index");
+
+// References the routers.
+var routes = {
+    index: require("./routes/index")
+};
+
+// Initializes a new Express application.
 var app = express();
 
 // Express settings.
 app.set("port", process.env.OPENSHIFT_NODEJS_PORT || "8080");
 app.set("ip address", process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
 app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
 // Gets the portuguese contents of the page.
 var contents = JSON.parse(require('fs').readFileSync("./public/contents/pt.json"));
 
-// Sets the router.
-app.use("/", router);
+// Sets the index router.
+app.use("/", router.index);
 
+// Sets the routes of the static files.
+app.use("/public", express.static(__dirname + "/public"));
+
+// Fly, app! Fly!
 app.listen(app.get("port"), app.get("ip address"), function () {
     console.log("Working @ " + app.get("ip address") + ":" + app.get("port") + "!");
 });
