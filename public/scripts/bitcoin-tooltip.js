3c0944521c5f2a0ea2b6b5bb34b73a7ac13b754b
Rafael Escobar <rafael@nhanderu.com>
2015-06-25 22:12:26 -0300

    Add tooltip in bitcoin menu item.

---
+++ public/scripts/bitcoin-tooltip.js
@@ -0,0 +1,8 @@
+document.getElementById("bitcoin-activator").addEventListener("click", function (e) {
+	var btc = document.getElementById("bitcoin-text");
+	
+	if (btc.style.opacity == "1")
+		btc.style.opacity = "0";
+	else
+		btc.style.opacity = "1";
+}, false);
