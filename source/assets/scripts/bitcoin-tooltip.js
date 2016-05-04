f375301c286ebfc1a2ef4eba8dbc16f6a4a2d315
Rafael Escobar <rafael@nhanderu.com>
2016-05-04 15:15:48 -0300

    Adjust the IDs on BTC script.

--- source/assets/scripts/bitcoin-tooltip.js
+++ source/assets/scripts/bitcoin-tooltip.js
@@ -1,8 +1,8 @@
-document.getElementById("bitcoin-activator").addEventListener("click", function (e) {
-	var btc = document.getElementById("bitcoin-text");
+document.getElementById('bitcoin-activator').addEventListener('click', function (e) {
+	var btc = document.getElementById('bitcoin-box');
 	
-	if (btc.style.opacity == "1")
-		btc.style.opacity = "0";
+	if (btc.style.opacity == '1')
+		btc.style.opacity = '0';
 	else
-		btc.style.opacity = "1";
+		btc.style.opacity = '1';
 }, false);
