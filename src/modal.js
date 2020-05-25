f47acbb503d7abd81895b5a12414303b070f854f
Rafael Escobar <rescobar@protonmail.com>
2020-05-24 23:51:42 -0300

    Fix modal bug
    Created modal background to exit the modal whenever its clicked.

--- src/modal.js
+++ src/modal.js
@@ -1,24 +1,26 @@
 const modal = window['modal'] || document.getElementById('modal')
 const modalLink = window['modal-link'] || document.getElementById('modal-link')
 const modalCloser = window['modal-closer'] || document.getElementById('modal-closer')
+const modalBackground = window['modal-background'] || document.getElementById('modal-background')
 
 const isModalOpen = () => !!Number(modal.style.opacity)
 
 const closeModal = () => {
     modal.style.opacity = 0
     modal.style.pointerEvents = 'none'
+    modalBackground.style.opacity = 0
+    modalBackground.style.pointerEvents = 'none'
 }
 
 const openModal = () => {
     modal.style.opacity = 1
     modal.style.pointerEvents = 'all'
+    modalBackground.style.opacity = 1
+    modalBackground.style.pointerEvents = 'all'
 }
 
-window.addEventListener('click', event => {
-    if (event.target == modalLink)
-        isModalOpen() ? closeModal() : openModal()
-    else if (event.target == modalCloser)
-        closeModal()
-    else if (event.target != modal)
-        isModalOpen() && closeModal()
-})
+modalLink.addEventListener('click', () => isModalOpen() ? closeModal() : openModal())
+modalCloser.addEventListener('click', closeModal)
+modalBackground.addEventListener('click', closeModal)
+
+closeModal()
