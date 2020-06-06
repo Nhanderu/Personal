9a84029531a7afd37ecd96409ddb0b0da8ba44d4
Rafael Escobar <rescobar@protonmail.com>
2020-06-06 16:22:43 -0300

    Fix Chrome version
    Fix style on Chrome's mobile version, specifically.

---
+++ src/chrome-mobile.js
@@ -0,0 +1,6 @@
+if (chrome) {
+    document.querySelector('body').classList.add('webkit-mobile-full-height')
+    document.querySelector('.wrapper').classList.add('webkit-mobile-full-height')
+    document.querySelector('#modal').classList.add('webkit-mobile-full-height')
+    document.querySelector('#modal-background').classList.add('webkit-mobile-full-height')
+}
