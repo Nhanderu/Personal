37f05b4f609c9bdeec0aac708a855dd5a56bef19
Rafael Escobar <rescobar@protonmail.com>
2020-08-08 21:36:15 -0300

    Fix birthday calculation

--- src/date-update.js
+++ src/date-update.js
@@ -1,5 +1,5 @@
 const msToYears = ms => ms / (1000 * 60 * 60 * 24 * (365 + 365 + 365 + 366) / 4)
-const ms = new Date() - new Date(1996, 7, 27)
+const ms = new Date() - new Date(1996, 6, 27)
 const years = Math.floor(msToYears(ms))
 
 const age = window['age-text'] || document.getElementById('age-text')
